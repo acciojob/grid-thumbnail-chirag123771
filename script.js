371e91e2c4dccbf931bc6bba83b0ae152f8b1cc4
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-02-05 01:04:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ function expandImage(thumbnailId) {
+        // Get the source of the clicked thumbnail
+        const thumbnailSrc = document.getElementById(thumbnailId).src;
+
+        // Set the source of the expanded image
+        document.getElementById('expandedImage').src = thumbnailSrc;
+
+        // Display the full-size image and hide the other thumbnails
+        document.getElementById('fullsize').style.display = 'flex';
+        document.getElementById('thumbnailGrid').style.display = 'none';
+    }
+
+    function collapseImage() {
+        // Hide the full-size image and display the thumbnail grid
+        document.getElementById('fullsize').style.display = 'none';
+        document.getElementById('thumbnailGrid').style.display = 'grid';
+    }
